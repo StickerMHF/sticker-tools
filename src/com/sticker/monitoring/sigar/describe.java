0c66518ba2fa66344b11f2e0b63a47a4fd694b8e
MaoHaifeng <mhfflying@163.com>
2017-07-24 14:33:27 +0800

    Revert "Revert "V0.1""
    This reverts commit 0b68a87fa3fdd2cc007932064fffb2f169eda2d1.

---
+++ src/com/sticker/monitoring/sigar/describe.java
@@ -0,0 +1,10 @@
+package com.sticker.monitoring.sigar;
+
+public class describe {
+//记得配置项目工程的.classpath文件
+	/*<classpathentry kind="src" path="src">
+	<attributes>
+		<attribute name="org.eclipse.jdt.launching.CLASSPATH_ATTR_LIBRARY_PATH_ENTRY" value="E:/06-Sticker/01-服务器监控/sigar/lib"/>
+	</attributes>
+</classpathentry>*/
+}
